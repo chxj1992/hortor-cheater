001a9230426754b90c1d1d94593d7e455b8f09c1
chxj1992 <chxj1992@gmail.com>
2018-01-23 13:24:21 +0800

    add agent header

--- listen.py
+++ listen.py
@@ -4,12 +4,12 @@ from mitmproxy import ctx
 from urllib.parse import quote
 import string
 import requests
+import webbrowser
 
 def response(flow):
     path = flow.request.path
     if path == '/question/bat/findQuiz':
-        content = flow.response.content
-        data = json.loads(content)
+        data = json.loads(flow.response.text)
         question = data['data']['quiz']
         options = data['data']['options']
         ctx.log.info('question : %s, options : %s'%(question, options))
@@ -19,8 +19,9 @@ def response(flow):
 
 
 def ask(question, options):
-    url = quote('http://www.baidu.com/s?wd=' + question, safe = string.printable)
-    content = requests.get(url).text
+    url = quote('https://www.baidu.com/s?wd=' + question, safe = string.printable)
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"}
+    content = requests.get(url, headers=headers).text
     answer = []
     for option in options:
         count = content.count(option)
